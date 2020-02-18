16bf74d9fda13864c5fce02240fc64cb1daf9239
cojmar <cojmar@gmail.com>
2020-02-18 03:29:41 +0200

    ..

--- tools/build.js
+++ tools/build.js
@@ -5,17 +5,28 @@
     optimize: "none", // disable standard uglifyjs optimisations
     onBuildWrite:function(moduleName, path, contents) {     
         let terser = require.nodeRequire("terser");
-        let options = {
-            toplevel: true,
+        let options = {      
+            parse: {
+                ecma: 5, // specify one of: 5, 2015, 2016, 2017 or 2018
+            },      
             compress: {                
                 passes: 3
             },            
-            mangle: true,
-            output: {
+            mangle: true,            
+            output: {                
                 comments:false,
                 beautify: false,
                 preamble: ""
-            }
+            },
+            ecma: 5, // specify one of: 5, 2015, 2016, 2017 or 2018
+            keep_classnames: false,
+            keep_fnames: false,
+            ie8: false,
+            module: false,
+            nameCache: null, // or specify a name cache object
+            safari10: false,
+            toplevel: false,
+            warnings: false,
         };
 
         let result = terser.minify(contents,options);
